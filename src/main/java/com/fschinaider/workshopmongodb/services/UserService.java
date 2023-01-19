77c0101b7f271bd71df65823db18e90b91e99cf3
Felipe Schinaider <felipeschinaidernt@gmail.com>
2023-01-19 07:29:03 -0400

    User POST, DELETE, PUT

--- src/main/java/com/fschinaider/workshopmongodb/services/UserService.java
+++ src/main/java/com/fschinaider/workshopmongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.fschinaider.workshopmongodb.domain.User;
+import com.fschinaider.workshopmongodb.dto.UserDTO;
 import com.fschinaider.workshopmongodb.repositories.UserRepository;
 import com.fschinaider.workshopmongodb.services.exception.ObjectNotFoundException;
 
@@ -19,9 +20,33 @@ public class UserService {
 	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado"));
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
+
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
 	}
 }
