557021e0fb33c341f9b2f5b8ea4626ed804f2e71
Felipe Schinaider <felipeschinaidernt@gmail.com>
2023-01-19 06:32:50 -0400

    Obtendo usuario por id

--- src/main/java/com/fschinaider/workshopmongodb/resources/UserResource.java
+++ src/main/java/com/fschinaider/workshopmongodb/resources/UserResource.java
@@ -5,10 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import com.fschinaider.workshopmongodb.domain.User;
 import com.fschinaider.workshopmongodb.dto.UserDTO;
 import com.fschinaider.workshopmongodb.services.UserService;
@@ -20,10 +17,16 @@ public class UserResource {
 	@Autowired
 	private UserService service;
 
-	@RequestMapping(method=RequestMethod.GET)
+	@GetMapping
  	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@GetMapping("/{id}")
+ 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
 }
